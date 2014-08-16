711ace8d17751d459b8e47a3f1d7e049d3c32ad7
belkalah-mbp-xu <belkacem.lahouel@gmail.com>
2014-08-16 13:44:27 +0200

    Adding Queue data-structure. Valgrind OK. Small modifications in Stack

--- data_structures/queue/main.c
+++ data_structures/queue/main.c
@@ -4,25 +4,25 @@
  * 08/2014
  * belkacem.lahouel@utbm.fr
  *
- * Code for classic and simple Stack of int.
+ * Code for classic and simple Queue of int.
  ***/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /***
- * Definitions for Node and Stack data-structures.
+ * Definitions for Node and Queue data-structures.
  ***/
 
 typedef struct _Node {
     int value;
     struct _Node* next;
-} Node;
+} Node;	
 
-typedef struct _Stack {
+typedef struct _Queue {
     Node* top;
     int nb_nodes;
-} Stack;
+} Queue;
 
 /***
  * Definition of a new data-type: Boolean.
@@ -47,7 +47,7 @@ Node* create_node(int _value) {
 /***
  * Removal of a Node and memory freeing.
  * Removes the Node and returns the address on the next Node.
- * This is more convenient when coming to Stack management.
+ * This is more convenient when coming to Queue management.
  ***/
 
 Node* remove_node(Node* _node) {
@@ -61,12 +61,12 @@ Node* remove_node(Node* _node) {
 }
 
 /***
- * Checks whether the Stack is empty or not.
- * TRUE means that the Stack is empty.
- * FALSE means that the Stack is not empty.
+ * Checks whether the Queue is empty or not.
+ * TRUE means that the Queue is empty.
+ * FALSE means that the Queue is not empty.
  ***/
 
-Boolean is_empty(Stack _ll) {
+Boolean is_empty(Queue _ll) {
     if (_ll.nb_nodes <= 0) {
         return TRUE;
     }
@@ -74,23 +74,23 @@ Boolean is_empty(Stack _ll) {
 }
 
 /***
- * Creation of an empty Stack.
- * Returns this newly created Stack.
+ * Creation of an empty Queue.
+ * Returns this newly created Queue.
  ***/
 
-Stack init_stack() {
-    Stack new_ll;
+Queue init_queue() {
+    Queue new_ll;
     new_ll.top = NULL;
     new_ll.nb_nodes = 0;
     return new_ll;
 }
 
 /***
- * Removal of all Nodes in the Stack.
- * Returns this updated and empty Stack.
+ * Removal of all Nodes in the Queue.
+ * Returns this updated and empty Queue.
  ***/
 
-Stack empty_stack(Stack _ll) {
+Queue empty_queue(Queue _ll) {
     Node* tmp = _ll.top;
     while (tmp != NULL) {
         tmp = remove_node(tmp);
@@ -101,11 +101,11 @@ Stack empty_stack(Stack _ll) {
 }
 
 /***
- * Adding an int in the head of the Stack.
- * Returns the updated Stack.
+ * Adding an int in the head of the Queue.
+ * Returns the updated Queue.
  ***/
 
-Stack push(Stack _ll, int _new_value) {
+Queue enqueue(Queue _ll, int _new_value) {
     Node* new_node = create_node(_new_value);
     new_node->next = _ll.top;
     ++ _ll.nb_nodes;
@@ -114,23 +114,32 @@ Stack push(Stack _ll, int _new_value) {
 }
 
 /***
- * Removes the head (top) of the Stack (first Node).
- * Returns a pointer on the updated LinkedList.
+ * Removes the tail of the Queue (last Node).
+ * Returns a pointer on the updated Queue.
  ***/
 
-Stack pop(Stack _ll) {
-    if (is_empty(_ll) == FALSE) {
-        _ll.top = remove_node(_ll.top);
-        -- _ll.nb_nodes;
-    }
-    return _ll;
+Queue dequeue(Queue _ll) {
+	if (is_empty(_ll) == FALSE) {
+		Node* tmp = _ll.top;
+		if (tmp->next != NULL) {
+			for (tmp; tmp->next->next != NULL; tmp = tmp->next) {}
+			tmp->next = remove_node(tmp->next);
+			tmp->next = NULL;
+		} else {
+			tmp = remove_node(tmp);
+			_ll.top = tmp;
+		}
+		-- _ll.nb_nodes;
+	}
+
+	return _ll;
 }
 
 /***
  * Prints the whole Stack.
  ***/
 
-void print_stack(Stack _ll) {
+void print_queue(Queue _ll) {
     if (is_empty(_ll) == FALSE) {
         Node* tmp = _ll.top;
         printf("%d", tmp->value);
@@ -156,28 +165,28 @@ void print_stack(Stack _ll) {
  ***/
 
 int main(int argc, char** argv) {
-    Stack tmp_ll = init_stack();
+    Queue tmp_ll = init_queue();
 
-    tmp_ll = pop(tmp_ll);
-    print_stack(tmp_ll);
+    tmp_ll = dequeue(tmp_ll);
+    print_queue(tmp_ll);
 
-    tmp_ll = push(tmp_ll, 13);
-    print_stack(tmp_ll);
+    tmp_ll = enqueue(tmp_ll, 13);
+    print_queue(tmp_ll);
 
-    tmp_ll = push(tmp_ll, 14);
-    print_stack(tmp_ll);
+    tmp_ll = enqueue(tmp_ll, 14);
+    print_queue(tmp_ll);
 
-    tmp_ll = pop(tmp_ll);
-    print_stack(tmp_ll);
+    tmp_ll = dequeue(tmp_ll);
+    print_queue(tmp_ll);
 
-    tmp_ll = push(tmp_ll, 17);
-    print_stack(tmp_ll);
+    tmp_ll = enqueue(tmp_ll, 17);
+    print_queue(tmp_ll);
 
-    tmp_ll = pop(tmp_ll);
-    print_stack(tmp_ll);
+    tmp_ll = dequeue(tmp_ll);
+    print_queue(tmp_ll);
 
-    tmp_ll = empty_stack(tmp_ll);
-    print_stack(tmp_ll);
+    tmp_ll = empty_queue(tmp_ll);
+    print_queue(tmp_ll);
 
     return EXIT_SUCCESS;
 }
